72b1af52718d1481dabd7f1f282c9b7b9c0ec750
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2023-10-09 13:33:05 +0530

    Added argumentparser

--- segmentation/train_model.py
+++ segmentation/train_model.py
@@ -1,12 +1,22 @@
 from ultralytics import YOLO
+import argparse
 
 
-def train_model():
+def train_model(config_path: str = 'segmentation/config.yaml', epochs: int = 5, imgsz: int = 224):
     model = YOLO('yolov8n.pt')
-    results = model.train(data='segmentation/config.yaml', epochs=5, imgsz=224)
+    results = model.train(data=config_path, epochs=epochs, imgsz=imgsz)
     return results
 
 
-if __name__ == '__main__':
-    results = train_model()
-    print(results)
+parser = argparse.ArgumentParser(description="Training Script for YOLO")
+parser.add_argument('--config_path')
+parser.add_argument('--epochs')
+parser.add_argument('--imgsz')
+
+args = parser.parse_args()
+config_path = args.config_path
+epochs = args.epochs
+image_size = args.imgsz
+
+results = train_model(config_path, epochs, image_size)
+print(results)
